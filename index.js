201168c1975b9077ccb724bc40c887dfad8db1b6
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-09-17 16:46:07 -0700

    student

--- index.js
+++ index.js
@@ -180,8 +180,19 @@ console.log(studentOne.speak())
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian{
+constructor(attributes){
+  super(attributes);
+  this.specialty= attributes.specialty;
+  this.faveLanguage = attributes.faveLanguage; //-------!
+  this.catchPhrase= attributes.catchPhrase;
+}
+demo(subject){
+  return `Today we are learning about ${subject}`
+}
+grade(student, subject){
+return `${student.name} recieves a perfect score on ${subject}`
+}
 }
 
 /*
@@ -199,8 +210,20 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian{
+  constructor(attributes){
+  super(attributes);
+  this.previousBackground = attributes.previousBackground;
+  this.className = attributes.className;
+  this.faveSubjects = [];
+  }
+listSubjects(){
+  return `${this.faveSubjects.length}`
+}
+PRAssignment(subject){
+  return `${this.name} has submitted a PR for ${subject}`
+}
+sprintChallenge 
 }
 
 /*
